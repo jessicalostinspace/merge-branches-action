9323bc2a5cc33511e51f87360730d654be78a54a
Jessica Wilson <jwilson@zipwhip.com>
2020-01-23 15:19:41 -0800

    added debugstring

--- merge-branches.sh
+++ merge-branches.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-set -eou pipefail
+set -x
 
 branchName=$1
 baselineBranch=$2
+repositoryUrl=$3
 
 echo =================================================
 echo  MERGING RELEASE $branchName INTO $baselineBranch          
 echo =================================================
 
+remoteBranch=$(git branch -r | grep $branchName)
+echo $remoteBranch
+
 git fetch
 git checkout $baselineBranch
+git remote set-url origin $repositoryUrl
 git pull origin $baselineBranch
-git merge $branchName
+git merge --no-ff $remoteBranch
 git push origin $baselineBranch
+
+echo "Success or Failure"
