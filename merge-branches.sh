631dc9818e35dba6304139bf3d6e940fdd700894
Jessica <jessicalostinspace@gmail.com>
2020-01-23 15:09:34 -0800

    Development (#5)

--- merge-branches.sh
+++ merge-branches.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-set -eou pipefail
+set -x
 
 branchName=$1
 baselineBranch=$2
+repositoryUrl=$3
 
 echo =================================================
 echo  MERGING RELEASE $branchName INTO $baselineBranch          
 echo =================================================
 
-echo git branch -r | grep $branchName
-git branch -r | grep $branchName
-# git fetch
-# git checkout $baselineBranch
-# git pull origin $baselineBranch
-# git merge origin $branchName
-# git push origin $baselineBranch
+remoteBranch=$(git branch -r | grep $branchName)
+echo $remoteBranch
+
+git fetch
+git checkout $baselineBranch
+git remote set-url origin $repositoryUrl
+git pull origin $baselineBranch
+git merge --no-ff $remoteBranch
+git push origin $baselineBranch
