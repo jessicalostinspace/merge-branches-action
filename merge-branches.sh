7d7ce7719359014ccdf6910d92a857dde0bc99e5
Jessica Wilson <jwilson@zipwhip.com>
2020-01-23 13:57:35 -0800

    Parsing remote branches

--- merge-branches.sh
+++ merge-branches.sh
@@ -8,8 +8,10 @@ echo =================================================
 echo  MERGING RELEASE $branchName INTO $baselineBranch          
 echo =================================================
 
-git fetch
-git checkout $baselineBranch
-git pull origin $baselineBranch
-git merge origin $branchName
-git push origin $baselineBranch
+echo git branch -r | grep $branchName
+git branch -r | grep $branchName
+# git fetch
+# git checkout $baselineBranch
+# git pull origin $baselineBranch
+# git merge origin $branchName
+# git push origin $baselineBranch
