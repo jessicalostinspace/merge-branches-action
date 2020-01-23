cc948d91973cbfb1d55971dc16613bc4dc5efc97
Jessica Wilson <jwilson@zipwhip.com>
2020-01-23 14:55:58 -0800

    Added repo url

--- merge-branches.sh
+++ merge-branches.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -eou pipefail
+# set -eou pipefail
 
 branchName=$1
 baselineBranch=$2
+repositoryUrl=$3
 
 echo =================================================
 echo  MERGING RELEASE $branchName INTO $baselineBranch          
@@ -13,6 +14,7 @@ echo $remoteBranch
 
 git fetch
 git checkout $baselineBranch
+git remote set-url origin $repositoryUrl
 git pull origin $baselineBranch
-git merge $remoteBranch
+git merge --no-ff $remoteBranch
 git push origin $baselineBranch
