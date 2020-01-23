6558895fe902247045cbce87c0bd1b5e8aaae192
Jessica Wilson <jwilson@zipwhip.com>
2020-01-23 14:18:57 -0800

    Merging remote branch

--- merge-branches.sh
+++ merge-branches.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# set -eou pipefail
+set -eou pipefail
 
 branchName=$1
 baselineBranch=$2
 
-# echo =================================================
-# echo  MERGING RELEASE $branchName INTO $baselineBranch          
-# echo =================================================
+echo =================================================
+echo  MERGING RELEASE $branchName INTO $baselineBranch          
+echo =================================================
 
-echo git branch -r | grep $branchName
-git branch -r | grep $branchName
-# git fetch
-# git checkout $baselineBranch
-# git pull origin $baselineBranch
-# git merge origin $branchName
-# git push origin $baselineBranch
+remoteBranch=$(echo $(git branch -r | grep $branchName))
+
+git fetch
+git checkout $baselineBranch
+git pull origin $baselineBranch
+git merge $remoteBranch
+git push origin $baselineBranch
