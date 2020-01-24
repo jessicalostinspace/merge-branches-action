a45f3fe90424f565c5d69a9b893e8fd44334b4de
Jessica Wilson <jwilson@zipwhip.com>
2020-01-23 17:11:18 -0800

    adding github actions as commiter

--- merge-branches.sh
+++ merge-branches.sh
@@ -15,6 +15,8 @@ echo $remoteBranch
 git fetch
 git checkout $baselineBranch
 git pull origin $baselineBranch
+git config --global user.email "github-actions@noreply.github.com.com"
+git config --global user.name "github-actions"
 git remote set-url origin $repositoryUrl
 git merge --no-ff $remoteBranch
 git push origin $baselineBranch
